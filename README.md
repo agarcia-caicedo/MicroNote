60a93cd7880320d44d95f5c1a422c9d8ddfc7e0a
Jack <nian.jack@gmail.com>
2019-09-12 11:00:31 -0400

    added autosave

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MicroNote
 A note taking web app.
+
+#Installation
+
+Clone the repo into your local machine
+```
+git clone https://github.com/nianjack/MicroNote.git
+```
+
+Copy and paste the index.html path into the URL bar of Chrome or Firefox.
--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 
-const fs = new Filer.FileSystem();
+    const fs = new Filer.FileSystem();
+    var noteData;
 
-fs.readFile('/note', 'utf8', function(err, data){
-    const msg = "Welcome to MicroNote";
+    fs.readFile('/note', 'utf8', (err, data) => {
+        const msg = "Welcome to MicroNote!";
 
-    if (err){
-        console.log(msg);
-        document.querySelector('#note').innerHTML = msg;
-    }
-    if (data){
-        console.log(data);
-        document.querySelector('#note').innerHTML = data;
-    }
-})
+        if (err) {
+            console.log(msg);
+            document.querySelector('#note').innerHTML = msg;
+        }
+        else {
+            console.log(data);
+            document.querySelector('#note').innerHTML = data;
+
+            setInterval(() => {
+                fs.writeFile('/note', data, (err) => {
+                    if (err) console.log("error: " + err);
+                    else{
+                        data = document.querySelector('#note').innerHTML;
+                        console.log("saving: " + data);
+                    }
+                });
+            }, 2000);
+        }
+    })
 });
+
